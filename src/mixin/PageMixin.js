9e88b451fff743fd2eaaef26d11521e0b2db84c0
crazyming <crazyming9528@qq.com>
2019-06-20 14:53:54 +0800

    页面滚动条自动到顶部

--- src/mixin/PageMixin.js
+++ src/mixin/PageMixin.js
@@ -10,14 +10,18 @@ const pageMixin = {
 
   },
   methods: {
-    //面包屑
+    /**
+     *从路由参数中  拼出 面包屑导航
+     */
     pageMixin_refreshBreadcrumb() {
       let matched = this.$route.matched.filter(item => item.meta && item.meta.title);
-      matched = [{path: '/', meta: {title: 'Home'}}].concat(matched);
+      matched = [{path: '/', meta: {title: 'Home'}}].concat(matched)
       let breadcrumb = matched;
       this.$store.commit('refreshBreadList', breadcrumb);
     },
-    // 十秒后 请求没有完成 就强行显示页面
+    /**
+     * 十秒后 请求没有完成 就强行显示页面
+     */
     pageMixin_forceShowPage() {
 
       setTimeout(() => {
@@ -28,6 +32,18 @@ const pageMixin = {
 
       }, 10000)
 
+    },
+    /**
+     * 切换页面后  进度条到顶部
+     */
+    pageMixin_toTop() {
+      window.scrollTo(0, 0);
+      setTimeout(function () {
+        var em = document.getElementsByClassName("el-main")
+        em[0].scrollTop = 0;
+        //console.log(em[0].scrollTop);
+      }, 10)
+
     }
 
   },
@@ -35,6 +51,9 @@ const pageMixin = {
     this.pageMixin_refreshBreadcrumb();
     this.pageMixin_forceShowPage();
 
+  },
+  mounted() {
+    this.pageMixin_toTop();
   }
 };
 
